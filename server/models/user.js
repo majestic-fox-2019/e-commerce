7015f098cc51270f9c0ef0bdf759431b5fa177df
marcel <marcell.maruli021@gmail.com>
2020-02-17 12:59:38 +0700

    first commit

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const { Model } = sequelize.Sequelize
+
+  class User extends Model {}
+
+  User.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    admin: DataTypes.BOOLEAN
+  }, { sequelize });
+  
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
