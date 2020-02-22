7003638ffc1bf1502a2320c4e027f6a1c1b46c00
Onesinus SPT <onesinus231@gmail.com>
2020-02-22 11:21:16 +0700

    Testing udah ganteng harusnya...

--- server/__tests__/Product.js
+++ server/__tests__/Product.js
@@ -2,23 +2,26 @@ const app       = require('../app');
 const supertest = require('supertest');
 const request   = supertest(app);
 let token       = '';
+const {compare} = require('../helpers/hash');
 
 describe('/products', () => {
-    it('GET / without token', async done => {
+    it('POST /register to make sure data test already registered', async done => {
+        let reqBody = {
+            email: "kodekite@gmail.com",
+            password: "123"
+        }
+        
         const response = await request
-            .get('/products')
-            expect(response.status).toBe(401);
+            .post('/users/register')
+            .send(reqBody)
+
+            expect([201, 400]).toContain(response.status);
+            expect(response.body.email).toBe(reqBody.email);
+            expect(compare(reqBody.password,response.body.password)).toBe(true);           
+            
             done();
     });
-
-    it('GET / with invalid token', async done => {
-        const response = await request
-            .get('/products')
-            .set('accesstoken', 'claywashere')
-            expect(response.status).toBe(401);
-            done();
-    });
-
+    
     it('POST /login with exist account to getting token for get api testing', async done => {
         let reqBody = {
             email: "kodekite@gmail.com",
@@ -32,6 +35,21 @@ describe('/products', () => {
             expect(response.body.token).not.toBe(undefined);
             token = response.body.token;
             
+            done();
+    });
+
+    it('GET / without token', async done => {
+        const response = await request
+            .get('/products')
+            expect(response.status).toBe(401);
+            done();
+    });
+
+    it('GET / with invalid token', async done => {
+        const response = await request
+            .get('/products')
+            .set('accesstoken', 'claywashere')
+            expect(response.status).toBe(401);
             done();
     });
 
@@ -62,8 +80,76 @@ describe('/products', () => {
                 stock: 75
             });
 
-            console.log(response.body);
             expect(response.status).toBe(201);
+            done();
+    });
+
+    it('POST / with valid token and un-complete data needed', async done => {
+        const response = await request
+            .post('/products')
+            .set('accesstoken', token)
+            .send({
+                name: '', 
+                image_url: 'www.lidikuy.com', 
+                price: 10000, 
+                stock: 75
+            });
+
+            expect(response.status).toBe(400);
+            done();
+    });
+
+    it('PUT / with invalid token', async done => {
+        const response = await request
+            .put('/products')
+            .set('accesstoken','cubluk')
+            expect(response.status).toBe(401);
+            done();
+    });
+
+    it('PUT / with valid token and complete data needed', async done => {
+        const response = await request
+            .put('/products/1')
+            .set('accesstoken', token)
+            .send({
+                name: 'Sapu lidi', 
+                image_url: 'www.lidikuy.com', 
+                price: 10000, 
+                stock: 75
+            });
+
+            expect(response.status).toBe(200);
+            done();
+    });
+
+    it('PUT /:id with valid token and un-complete data needed', async done => {
+        const response = await request
+            .put('/products/1')
+            .set('accesstoken', token)
+            .send({
+                name: '', 
+                image_url: 'www.lidikuy.com', 
+                price: 10000, 
+                stock: 75
+            });
+
+            expect(response.status).toBe(400);
+            done();
+    });
+
+    it('DELETE / with invalid token', async done => {
+        const response = await request
+            .delete('/products')
+            .set('accesstoken','cubluk')
+            expect(response.status).toBe(401);
+            done();
+    });
+
+    it('DELETE / with valid token', async done => {
+        const response = await request
+            .delete('/products/1')
+            .set('accesstoken',token)
+            expect(response.status).toBe(200);
             done();
     });
 });
