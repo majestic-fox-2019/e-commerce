acc6e8fa124afefd674a396d8095683aea7aacbf
purandrar <andra_pur@yahoo.com>
2020-02-22 12:05:42 +0700

    ready to push to heroku

--- server/app.js
+++ server/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const request = require("supertest");
 const Routes = require("./routes/index");
 const cors = require('cors')
+const port = process.env.PORT || 3000
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
 app.use("/", Routes);
 module.exports = app;
-app.listen(3000, () => console.log("running"));
+app.listen(port, () => console.log("running"));
