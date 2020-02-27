b44d52030921c65917fc66c1b8e6f55a456cfb8d
Mazda94 <m.alli9401@gmail.com>
2020-02-27 11:49:24 +0700

    update pointing

--- member/src/helper/api.js
+++ member/src/helper/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:3000' })
+// const api = axios.create({baseURL: 'http://localhost:3000'})
+const api = axios.create({ baseURL: 'https://web-e-commerce.herokuapp.com' })
+
 
 export default api
