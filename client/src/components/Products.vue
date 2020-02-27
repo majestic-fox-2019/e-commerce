6b3f0107b74719a2f18aad208d4a5b4bc2443c36
mustaqinIshak <mustaqinishak@gmail.com>
2020-02-27 12:11:50 +0700

    check time commit

--- client/src/components/Products.vue
+++ client/src/components/Products.vue
@@ -13,7 +13,7 @@
       <tbody>
         <tr v-for="(product, id) in products" :key="id">
           <th scope="row">{{product.id}}</th>
-          <th><img :src="product.image_url" class="picture img-thumbnail" alt="" width= "60" height="60"></th>
+          <th><img :src="product.image_url" class="picture img-thumbnail" alt="" width= "50" height="50"></th>
           <th>{{product.name}}</th>
           <th>{{product.price}}</th>
           <th>{{product.stock}}</th>
